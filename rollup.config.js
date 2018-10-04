09d607f83eb6aee80eaf08104fcdb7f8acc315a7
Ilya_Kuznetsov <Ilya_Kuznetsov@epam.com>
2018-10-04 16:00:46 +0300

    fix(rollup): remove fbjs from external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,20 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import fs from 'fs'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 
 export default {
   input: 'src/index.js',
-  external: ['react', 'prop-types', 'fbjs/lib/shallowEqual', 'fbjs/lib/invariant'],
+  external: ['react', 'prop-types'],
   plugins: [
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
     }),
+    resolve(),
+    commonjs(),
   ],
   output: [
     {
@@ -30,8 +34,6 @@ export default {
       globals: {
         react: 'React',
         'prop-types': 'PropTypes',
-        'fbjs/lib/shallowEqual': 'shallowEqual',
-        'fbjs/lib/invariant': 'invariant',
       },
       sourcemap: true,
       file: pkg['umd:main'],
